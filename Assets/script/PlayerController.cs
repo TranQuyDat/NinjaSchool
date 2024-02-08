25c61291b2778f19a6d7f039d222c801b3940009
TranQuyDat <datraumuong@gmail.com>
2024-02-09 03:32:46 +0700

    09/02/2024 update player controller

--- Assets/script/PlayerController.cs
+++ Assets/script/PlayerController.cs
@@ -5,17 +5,32 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
+    public float powerjump;
     public float damage;
     public Rigidbody2D rb;
     public Animator animator;
+    public bool isgrounded;
+    public Transform checkground;
+    public float radiusground;
+    public LayerMask layerMaskground;
+    bool isjumping;
     private void Update()
     {
-        rb.velocity = new Vector2(Input.GetAxis("Horizontal"), 0);
+        rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), rb.velocity.y);
+        checkGrounded();
+        jump();
+        if (isjumping)
+        {
+            animator.SetFloat("y_velocity",Mathf.Round(rb.velocity.y));
+        }
+        atk();
     }
     private void FixedUpdate()
     {
         move();
         flip();
+        
+        
     }
     public void move()
     {
@@ -24,10 +39,10 @@ public class PlayerController : MonoBehaviour
             animator.SetBool("walk", false);
             return;
         }
+        Vector3 pos = new Vector3(Input.GetAxisRaw("Horizontal"), transform.position.y, 0);
+        this.transform.position += pos * moveSpeed * Time.deltaTime;
         animator.SetBool("walk", true);
         
-        Vector3 pos = new Vector3(Input.GetAxis("Horizontal"),0,0);
-        rb.MovePosition(this.transform.position + pos * moveSpeed * Time.deltaTime);
     }
     public void flip()
     {
@@ -40,9 +55,49 @@ public class PlayerController : MonoBehaviour
         );
     }
 
+    public void jump()
+    {
+        if (isgrounded && (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.UpArrow)))
+        {
+            isjumping = true;
+            rb.velocity =  new Vector2 (rb.velocity.x ,powerjump);
+        }
+        if(isgrounded && !isjumping)
+        {
+            animator.SetBool("jump", false);
+        }else animator.SetBool("jump", true);
+    }
+    public void checkGrounded()
+    {
+        Collider2D circlehit = Physics2D.OverlapCircle(checkground.position, radiusground, layerMaskground);
+        if (circlehit !=null && circlehit.CompareTag("ground") && !isgrounded)
+        {
+            isgrounded = true;
+            return;
+        }
+        else if(circlehit == null  && isgrounded)
+        {
+            isgrounded = false;
+            isjumping = false;
+        }
+    }
+
+    public void atk()
+    {
+        if(Input.GetKeyDown(KeyCode.E)|| Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            animator.SetBool("atk", true);
+        }
+    }
 
     public void endAtack()
     {
         animator.SetBool("atk", false);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(checkground.position, radiusground);
     }
 }
